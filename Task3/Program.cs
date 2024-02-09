1e0125e541059b5fc3e8b71d8afd5bd14280b94a
Александр <asamatokhin@gmail.com>
2024-02-09 21:58:02 +0300

    добавлено задание 3

---
+++ Task3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3: Напишите программу, которая принимает
+// на вход целое число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+
+Console.Write("Введите число из отрезка [10, 99]: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 9 && number < 99)
+{
+    int leftDigit = number / 10;
+    int rightDigit = number % 10;
+    int newNumber = leftDigit > rightDigit ? leftDigit : rightDigit;
+    System.Console.WriteLine(newNumber);
+}
+else 
+{
+    System.Console.WriteLine("Вы ввели неверное число");
+}
